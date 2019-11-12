21c7b2197ed5086c298c703f214e310d3285898d
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-11-12 02:24:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,6 +38,10 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    self.find_by_name(name)
+    if self.find_by_name
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
   end
 end
